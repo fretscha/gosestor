0d3817b68b77d6233008f06a683db364f2dc7708
Frederic Tschannen <fretscha@umx.net>
2026-07-02 22:09:57 +0200

    chore: bootstrap gosestor go module

---
+++ doc.go
@@ -0,0 +1,4 @@
+// Package gosestor is a Caddy HTTP handler plugin that caches backend
+// session cookies server-side in a Redis-compatible store and presents the
+// client an opaque, rotatable proxy cookie.
+package gosestor
